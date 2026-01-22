df025bb4a35a18049b5d351492acf7a8db080324
savickuse <savickuse@users.noreply.github.com>
2026-01-22 03:08:07 +0200

    style: fix async handling

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 45
 module.exports = API;
+
+
+// API module for UmbrellaFramework
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 46
+module.exports = API;
